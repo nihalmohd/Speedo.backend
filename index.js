a009079c9c1531e02fbe27d707074fa9dc6cb7ff
nihalmohd <nihalmohd828@gmail.com>
2024-10-06 23:53:14 +0530

    Port issue fixed

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { UserRouter } from "./controller/UserRouter.js";
+import dotenv from 'dotenv';
 
 
 
 const app=express();
+dotenv.config();
 
-const PORT=5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/",UserRouter)
 app.listen(PORT,()=>{
